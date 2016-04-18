3a0c2ad12841b0101f431ad975f21399d4e57189
nicholasjorg <nicholasjorgensen@nicholass-mbp-2.home>
2016-04-18 15:04:30 +0200

    Merge mCheckboks

--- simpleBarChartVærgerRegionerMCheckboks.php
+++ simpleBarChartVærgerRegionerMCheckboks.php
@@ -119,8 +119,9 @@
 				filterJsonArr[addIndex].region = jsonarr[addIndex].region;
                 fadeOut().done(update);
 	 		}
-	 		else{//console.log("exists er true. Prøver at fjerne: "+name);
+	 		else{
 	 		filterJsonArr[i].region = null;
+
 	 		fadeOut().done(update);
             }
 	 		//console.log(Object.keys(filterJsonArr).length);
@@ -130,38 +131,48 @@
 
         update;
 
-		var update = function(){
+	var update = function(){
         d3.select("svg").remove();
 		var newData = new Array();
 		for(var h=0; h<Object.keys(filterJsonArr).length; h++){
-			console.log(h);
-			if(filterJsonArr[h].region != null){newData.push(filterJsonArr[h]);  }
+			if(filterJsonArr[h].region == "needschanging") {filterJsonArr[h].region = null}
+			if(filterJsonArr[h].region != null) { newData.push(filterJsonArr[h]); }
 		}
-
+		//Sorterer newData ascending order
+		newData.sort(function(a,b){
+			return parseFloat(a.antal) - parseFloat(b.antal);
+		});
+			
+		//Sætter attributter
+		var margin = {top: 10, right: 0, bottom: 10, left: 40};
+		var w = 500, h = 500;
+		
 		//Laver svg element til at komme figuren
-    	var svg = d3.select("#contentRow").append("svg").attr("id","graph");
-        //fadeIn();
-    	//Sætter attributter
-    	var w=500, h=500;
-    	svg.attr("width", w).attr("height", h);
 
-    	svg.selectAll("rect").data(newData).enter().append("rect")
-        .attr("id", function(d,i){return d.region})
-    	.attr("x",function(d,i){ return i*21 })
-    	.attr("y", function (d){ return h-(d.antal/10)})
-    	.attr("width", 20)
-    	.attr("height", function (d){ return (d.antal/10)})
-    	.attr("fill", "teal");
-
-    	svg.selectAll("text")
-    	.data(newData)
-    	.enter()
-    	.append("text")
-    	.text(function (d) { return d.region })
-    	.attr("x",function(d,i){ return i*21 })
-    	.attr("y", function (d){ return h-(d.antal/10)});
-    	}
-	</script>
+		var svg = d3.select("#contentRow").append("svg").attr("id","graph").attr("width", w).attr("height", h);
+		
+		//Laver scale
+		var min = newData[0].antal;
+		var max = newData[Object.keys(newData).length-1].antal;
+		var yScale = d3.scale.linear().domain([0,max]).range([h-margin.top-margin.bottom,margin.top]).nice();
+		var xScale = d3.scale.ordinal().domain(newData.map(function (d){return d.region})).rangeRoundBands([margin.left, w-margin.left-margin.right], 0.1); 
+	
+		//Tegner rectangels
+		svg.selectAll("rect").data(newData).enter().append("rect")
+		.attr("x",function(d,i){ return xScale(d.region)})
+		.attr("y", function (d){ return yScale(d.antal)})
+		.attr("width", xScale.rangeBand() )
+		.attr("height", function (d){ return yScale(0) - yScale(d.antal) })
+		.attr("fill", "teal");
+	
+		//Bygger y-akse
+		var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+		svg.append("g").attr("class", "axis").attr("transform","translate(0,"+(h-margin.top-margin.bottom)+")").call(xAxis);
+		var yAxis = d3.svg.axis().scale(yScale).orient("left").ticks(15);
+		svg.append("g").attr("class", "axis").attr("transform", "translate("+margin.left+",0)").call(yAxis);
+	
+	}
+</script>
 
     <script>
     //Tabbed menu javascript
